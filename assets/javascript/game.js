316ff98b6953158ab319c8d415d2342ed0bf9b47
Taneah Patterson <taneahpatterson@taneahs-mbp.attlocal.net>
2017-07-14 19:20:00 -0400

    added work so far

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+// var targetNumber= 70;
+
+// $("#number-to-guess").text(targetNumber);
+
+var counter = 0;
+var crystalImage = 0;
+
+$(".crystal-image").on("click", function(){
+	alert("You clicked a crystal");
+})
+
+
+
+
+
+
+
